1f9f686c817a478c1de266223f41d5ade54010e9
Caner Mollaoğlu <canermollaoglu@hotmail.com>
2025-10-31 21:16:53 +0300

    Machines Entity eklendi. Formda gösterildi.

--- OtomatYonetimSistemi/Form1.cs
+++ OtomatYonetimSistemi/Form1.cs
@@ -1,3 +1,5 @@
+using OtomatYonetimSistemi.Entities;
+
 namespace OtomatYonetimSistemi
 {
     public partial class Form1 : Form
@@ -5,6 +7,16 @@ namespace OtomatYonetimSistemi
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Machines> machines = new List<Machines>
+            {
+                new Machines { Name = "Otomat 1", Location = "Kat 1" },
+                new Machines { Name = "Otomat 2", Location = "Kat 2" },
+                new Machines { Name = "Otomat 3", Location = "Kat 3" }
+            };
         }
     }
 }
